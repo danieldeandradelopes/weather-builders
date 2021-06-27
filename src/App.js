7c473ee576bbd194f0893fbb5083fc8c7df8edfa
Daniel Lopes <danieldeandradelopes@gmail.com>
2021-06-27 18:52:11 -0300

    integration of weather api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import weatherAPI from "./services/weather";
 
 function App() {
+  const [coords, setCoords] = useState(null);
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    getCoordinates();
+  }, []);
+
+  useEffect(() => {
+    if (coords) {
+      const { latitude, longitude } = coords;
+      handleRequestToApi(latitude, longitude);
+    }
+  }, [coords]);
+
+  function getCoordinates() {
+    navigator.geolocation.getCurrentPosition((localization) => {
+      const { coords: { latitude, longitude } } = localization;
+      setCoords({ latitude, longitude });
+    });
+  }
+
+  async function handleRequestToApi(latitude, longitude) {
+    const { data } = await weatherAPI.get(`weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_WEATHER_KEY}`);
+    setWeather(data);
+  }
+
+  async function handleUpdateWeather() {
+    setWeather("");
+    getCoordinates();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather Now</h1>
+      <div>
+        {weather && weather.weather[0].main}
+      </div>
+      <button onClick={() => handleUpdateWeather()}>
+        Test
+      </button>
     </div>
   );
 }
