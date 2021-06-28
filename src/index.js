1db412f43676565a014c0db93a1b31420a031a0f
Daniel Lopes <danieldeandradelopes@gmail.com>
2021-06-27 22:14:07 -0300

    remove unnecessary items and added imports of css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import dayjs from "dayjs";
 import App from "./App";
+import "dayjs/locale/pt-br";
+
+dayjs.locale("pt-br");
 
 ReactDOM.render(
   <React.StrictMode>
