d195ecc0181f470808280d33a09a40639e6049e9
Daniel Lopes <danieldeandradelopes@gmail.com>
2021-06-27 22:12:23 -0300

    create Footer Component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div>
+        <h2>Builders Weather</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
